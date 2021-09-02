7965601d4362b06ebd21692905299d46130fc3c8
called_D <called.0xd@gmail.com>
2021-09-02 22:16:52 +0900

    add: layouts.base

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,9 +1,12 @@
-index
-@foreach($articles as $article)
-    <article>
-        <h2>{{ $article->title }}</h2>
-        <div>
-            {{ $article->content }}
-        </div>
-    </article>
-@endforeach
+@extends('layouts.base')
+
+@section('content')
+    @foreach($articles as $article)
+        <article>
+            <h2>{{ $article->title }}</h2>
+            <div>
+                {{ $article->content }}
+            </div>
+        </article>
+    @endforeach
+@endsection
