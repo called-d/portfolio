e85ce00e9c01f3eadd5bbe28426c9573600417f5
called_D <called.0xd@gmail.com>
2021-09-02 13:20:53 +0900

    test: published if 10 min ago

--- tests/Unit/ArticleTest.php
+++ tests/Unit/ArticleTest.php
@@ -23,5 +23,9 @@ class ArticleTest extends TestCase
 
         $this->assertNull(Article::draft()->find($article->id), 'published article is not draft.');
         $this->assertNotNull(Article::published()->find($article->id), 'published article is published');
+
+        $ago = now()->subMinutes(10);
+        $this->assertNotNull(Article::draft($ago)->find($article->id), 'is draft at 10 min ago');
+        $this->assertNull(Article::published($ago)->find($article->id), 'not published at 10 min ago');
     }
 }
