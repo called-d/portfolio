f297ddc0eb1597e53cb2415deb3bba911b286204
called_D <called.0xd@gmail.com>
2021-09-02 22:05:13 +0900

    test: 管理者には下書きが見える

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Feature;
 
 use App\Models\Article;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
 class ArticleTest extends TestCase
@@ -15,6 +17,8 @@ class ArticleTest extends TestCase
      */
     public function test_article_index()
     {
+        Auth::login(User::where('is_admin', false)->firstOrFail());
+
         $article = Article::factory()->create();
 
         $response = $this->get('/');
@@ -28,5 +32,13 @@ class ArticleTest extends TestCase
         $response = $this->get('/');
         // 公開したら見える
         $response->assertSee($article->title);
+
+        // 下書きをもう一つ作る
+        $article = Article::factory()->create();
+        // 下書きは見えない
+        $this->get('/')->assertDontSee($article->title);
+        Auth::login(User::where('is_admin', true)->firstOrFail());
+        // 管理者には見える
+        $this->get('/')->assertSee($article->title);
     }
 }
