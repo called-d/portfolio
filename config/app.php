bb06603938238c5c861998743561261e468ea939
called_D <called.0xd@gmail.com>
2021-09-07 02:34:59 +0900

    add: github corner

--- config/app.php
+++ config/app.php
@@ -56,6 +56,8 @@ return [
 
     'asset_url' => env('ASSET_URL', null),
 
+    'repo_url' => env('APP_REPOSITORY_URL', null),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
