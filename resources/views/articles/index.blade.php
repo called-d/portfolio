5f555624db7c1c11614e3f81b6c40c9389bb0860
called_D <called.0xd@gmail.com>
2021-09-07 23:59:30 +0900

    mod: markdown用にスタイル調整

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -16,11 +16,19 @@
     .portfolio_article.card {
         padding: 0 1em;
     }
+    .portfolio_article.card h2.card_title {
+        font-size: 1.2rem;
+    }
     .portfolio_article.large_card {
         max-width: 900px;
         margin: auto;
         margin-bottom: 5em;
         padding: 0 1em;
+        overflow: hidden;
+    }
+    .portfolio_article.large_card a {
+        color: revert;
+        text-decoration: revert;
     }
 </style>
 @endpush
@@ -29,7 +37,7 @@
 <div id="app">
     <portfolio-slider :articles='@json($articles)'>
         <template v-slot="{ article }">
-            <h4>@{{ article.title }}</h4>
+            <h2 class="card_title">@{{ article.title }}</h2>
             <div>@{{ article.content }}</div>
         </template>
         <template v-slot:card_ui="{ article }">
@@ -39,7 +47,7 @@
 
     @foreach($articles as $article)
     <article class="portfolio_article large_card" id="fallback__{{ $article->slug }}" v-once>
-        <h4>{{ $article->title }}</h4>
+        <h2>{{ $article->title }}</h2>
         @isset($article->metadata['script']){!! $article->metadata['script'] !!}@endisset
         @isset($article->metadata['md'])
             <div class="article_content" data-is-md v-once><pre>{{ $article->content }}</pre></div>
